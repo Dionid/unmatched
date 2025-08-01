f9a5b0c00229826006b90782cc92265d6cbfb73e
David Shek <ditreyw@gmail.com>
2025-08-01 12:13:52 +0800

    feat: yjs working

--- frontend/admin-panel/src/lib/immer-yjs/types.ts
+++ frontend/admin-panel/src/lib/immer-yjs/types.ts
@@ -1,7 +1,7 @@
-export type JSONPrimitive = string | number | boolean | null
+export type JSONPrimitive = string | number | boolean | null;
 
-export type JSONValue = JSONPrimitive | JSONObject | JSONArray
+export type JSONValue = JSONPrimitive | JSONObject | JSONArray;
 
-export type JSONObject = { [member: string]: JSONValue }
+export type JSONObject = { [member: string]: JSONValue };
 
 export interface JSONArray extends Array<JSONValue> {}
